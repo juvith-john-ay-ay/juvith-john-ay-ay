b40fa37ffb86d91cd1d3580af6df19b885862873
juvith-john-ay-ay <90749398+juvith-john-ay-ay@users.noreply.github.com>
2021-09-15 14:55:33 +0800

    Add files via upload

---
+++ https:/github.com/juvith-john-ay-ay/Tetrisscr.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <random>
+#include "Block.h"
+#include "Tetromino.h"
+#include "Inputstate.h"
+
+
+class Game;
+
+class Tetrisscr : public sf::Drawable, public sf::Transformable
+// Handles the tetrominos and the whole play area
+{
+public:
+// Public methods
+  Tetrisscr(const sf::Vector2f& position, const sf::Vector2f& blockSize = { 32.0f,32.0f });
+void update(Game* target, const float dt);
+void handleInput();
+void setInputstate(PlayInputstate& state);
+void moveTetromino(const sf::Vector2i& dir);
+void dropTetromino();
+void rotateTetromino(const RotationDirection direction);
+
+private:
+// Private methods
+virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+void clearScreen();
+void scanLines();
+void destroyLine(const unsigned y);
+void advanceTimer(const float dt);
+void resetTimer();
+void skipStep();
+void drawTetromino();
+void newTetromino();
+void plantTetromino();
+void reset();
+
+bool canMove(const sf::Vector2i& dir);
+bool canRotate(const RotationDirection direction);
+bool isGameOver();
+
+private:
+//Private members
+static constexpr unsigned width{ 10 };
+static constexpr unsigned height{ 20 };
+static constexpr unsigned borderWidth{ 4 };
+static constexpr unsigned borderHeight{ 4 };
+static constexpr float updateDelay{ 30.0f };
+
+const sf::Vector2i spawnPosition{ 4,-2 };
+const sf::Color boardColor{ sf::Color::Black };
+const sf::Color backgroundColor{ sf::Color::Red };
+float updateTimer{ 1.0f };	
+  sf::RectangleShape background;	
+  Block gameBoard[height][width];	
+  Tetromino* curTetromino;	
+  PlayInputstate* inputState{ &PlayInputstate::idle };	
+  std::random_device rd;	
+  std::mt19937 rng{ rd() };	
+  std::uniform_int_distribution<int> uni{ 2, 6 };
+};
+
